d742b5d4f736acc47131245c00d207875b6f3236
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-11 14:05:29 -0500

    Demo configs

--- src/main/scala/cs441/project/cloudsim/mapreduce/ConfigLoader.scala
+++ src/main/scala/cs441/project/cloudsim/mapreduce/ConfigLoader.scala
@@ -6,14 +6,18 @@ import com.typesafe.config.ConfigFactory
 object ConfigLoader {
   val configs = ConfigFactory.load("MapReduceJob")
 
-  val INPUT_FILE_SIZE = configs.getInt("inputFileSize") * 1024 * 1024 * 1024 //Converting Gigs to bytes
+  val path = "MapReduce"
+
+  val INPUT_FILE_SIZE = configs.getInt(path + ".inputFileSize") * 1024 * 1024 * 1024 //Converting Gigs to bytes
 
 
-  val INPUT_SPLIT_SIZE = configs.getInt("inputSplitSize")
+  val INPUT_SPLIT_SIZE = configs.getInt(path + ".inputSplitSize")
 
-  val MAP_TASK_LENGTH = configs.getLong("mapLength")
+  val MAP_TASK_LENGTH = configs.getLong(path + ".mapLength")
 
-  val WORKER_NODES = configs.getList("VMS")
+  val WORKER_NODES = configs.getList(path + ".vms")
+
+  val NUMBER_OF_WORKER_NODES = configs.getInt(path + ".numberOfNodesAvailable")
 
 
 }
