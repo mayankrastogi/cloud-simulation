a844a6a58d6e2ac34578d95ab238415f91b7e69d
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-05-11 03:14:28 -0500

    WIP: added application master simulation co-ordinating mappers and reducers

--- src/main/scala/cs441/project/cloudsim/mapreduce/ResourceManager.scala
+++ src/main/scala/cs441/project/cloudsim/mapreduce/ResourceManager.scala
@@ -1,10 +1,11 @@
 package cs441.project.cloudsim.mapreduce
 
 import com.typesafe.config.Config
-import cs441.project.cloudsim.Job
+import cs441.project.cloudsim.jobs.Job
 import org.cloudbus.cloudsim.brokers.DatacenterBroker
 import org.cloudbus.cloudsim.cloudlets.Cloudlet
 import org.cloudbus.cloudsim.cloudlets.network.NetworkCloudlet
+import org.cloudbus.cloudsim.core.Simulation
 import org.cloudbus.cloudsim.vms.Vm
 
 /**
@@ -14,15 +15,57 @@ import org.cloudbus.cloudsim.vms.Vm
 
 class ResourceManager extends Job {
 
-  val mapper = new Mapper
-  val reducer = new Reducer
 
-  def allocateMappers(numberOfMappers: Int, inputSplitSize: Int): List[NetworkCloudlet] = {
+  //  val reducer = new Reducer
+  val MAPPER_PES = 2
+  val REDUCER_PES = 2
 
+  var simulation: Simulation = _
+
+  var mappers: List[Mapper] = _
+  var reducers: List[Reducer] = _
+
+  def allocateMappers(numberOfMappers: Int, inputSplitSize: Int, mapperSize: Long): List[Mapper] = {
+    (1 to numberOfMappers).map(iMapper => new Mapper(iMapper, inputSplitSize, MAPPER_PES).run(mapperSize)).toList
 
   }
 
-  def allocateReducers(numberOfReducers: Int): List[NetworkCloudlet] = {}
+
+  /**
+    * Returns the associated mapper with given reducer Id
+    *
+    * @param iReducer the reducer ID
+    * @return the mapper which emitted its input
+    */
+  def getAssociatedMapper(iReducer: Int): NetworkCloudlet = {
+    mappers.filter(mapper => mapper.getMapperId == iReducer).head.getMapperCloudlet
+
+  }
+
+  /**
+    * Returns the associated reducer with given mapper id
+    *
+    * @param iMapper the mapperID
+    * @return the reducer who will take the given mapper's output
+    */
+  def getAssociateReducer(iMapper: Int): NetworkCloudlet = {
+    reducers.filter(reducer => reducer.getReducerId == iMapper).head.getReducerCloudlet
+  }
+
+  def allocateReducers(numberOfReducers: Int, reducerLength: Long): List[Reducer] = {
+
+    val NumberOfPackets = 20 // Todo get the right number of packets
+
+
+    (1 to numberOfReducers).map(iReducer => new Reducer(iReducer, reducerLength, REDUCER_PES)).toList
+
+  }
+
+
+  def configureWorkerNodes() = {
+
+
+  }
 
   /**
     * Receives the Job requests and schedules the job tasks among the worker nodes.
@@ -36,18 +79,37 @@ class ResourceManager extends Job {
     val inputFileSize = ConfigLoader.INPUT_FILE_SIZE
     val inputSplitSize = ConfigLoader.INPUT_SPLIT_SIZE
 
-    // Decide on the number mappers and reducers based on the Input file size and the split size
+    // Decide on the number of mappers and reducers based on the Input file size and the split size
 
     val numberOfMappers, numberOfReducers = inputFileSize / inputSplitSize
 
-    // Allocate mappers job
-    val mappers = allocateMappers(numberOfMappers, inputSplitSize)
-
-    //Allocate reducers job
-    val reducers = allocateReducers(numberOfReducers)
-
 
     //Define the Vms/Resources on which mappers and reducers are simulated
+    configureWorkerNodes()
+
+
+    //TODO : Decide on the mapperSize and its relation with the inputFileSize
+    val individualMapperSize = 1024
+
+    // Allocate mappers job
+    mappers = allocateMappers(numberOfMappers, inputSplitSize, individualMapperSize)
+
+    //Todo : Decide on the reducer length
+    val reducerLength = 30
+
+    reducers = allocateReducers(numberOfReducers, reducerLength)
+
+    //Todo: Decide on the result size
+    val resultSize = 1024
+    mappers.foreach(mapper => mapper.persistAndCommunicateMapResponse(getAssociateReducer(mapper.getMapperId), resultSize))
+
+    //Allocate reducers job
+
+
+    //Todo: Decide on memory allocated and number of packets
+    val memoryAllocated = 1024
+    val numberOfPacketsToReceive = 20
+    reducers.map(reducer => reducer.run(memoryAllocated, getAssociatedMapper(reducer.getReducerId), numberOfPacketsToReceive))
 
 
   }
@@ -60,7 +122,9 @@ class ResourceManager extends Job {
     * @param broker   Reference to the datacenter broker that the job may use for dynamic creation/submission of
     *                 cloudlets and VMs.
     */
-  override def setSimulation(configId: Int, broker: DatacenterBroker): Unit = ???
+  override def setSimulation(configId: Int, broker: DatacenterBroker, simulation: Simulation): Unit = {
+    this.simulation = simulation
+  }
 
   /**
     * The VMs that this job needs for its execution.
